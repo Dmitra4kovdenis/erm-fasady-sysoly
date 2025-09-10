17878b09803020191b4ff66d7dbc8814cd2d0019
ivandmitrackov <dmitrachkovivan@yandex.ru>
2025-09-10 12:49:28 +0300

    поправил сборку

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   sassOptions: {
-    additionalData: '@import "styles/tokens.scss";',
+    includePaths: ["./src/**/*.scss"],
   },
 };
 
