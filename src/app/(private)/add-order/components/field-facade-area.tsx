ea8a36e8e76fc32f999a05c05ecedba4a9f12f2a
ivandmitrackov <dmitrachkovivan@yandex.ru>
2025-09-24 20:24:02 +0300

    логика расчета площади

--- src/app/(private)/add-order/components/field-facade-area.tsx
+++ src/app/(private)/add-order/components/field-facade-area.tsx
@@ -5,12 +5,15 @@ import { useEffect } from "react";
 export function FieldFacadeArea({ index }: { index: number }) {
   const { watch, setValue } = useFormContext();
 
+  // слушаем указанные поля
   const height = +watch(`items.${index}.height`);
   const width = +watch(`items.${index}.width`);
   const count = +watch(`items.${index}.count`);
 
+  // площадь - произведение этих значений
   const value = height * width * count;
 
+  // при изменении площади записываем её в поле
   useEffect(() => {
     setValue(`items.${index}.area`, value);
   }, [index, setValue, value]);
