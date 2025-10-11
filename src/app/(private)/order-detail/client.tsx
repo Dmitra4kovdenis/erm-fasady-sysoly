84d7a9c43bac61b809048f4fb22b570fa6735e25
Dmitrackov_Denis <Dmitra4kovdenis@gmail.com>
2025-10-11 20:46:45 +0300

    Табы в модалке

--- src/app/(private)/order-detail/client.tsx
+++ src/app/(private)/order-detail/client.tsx
@@ -1,12 +1,20 @@
 "use client";
 
 import { OrderDetailType } from "@/prisma-helpers/get-order-detail";
-import { Box, Dialog, DialogContent, DialogTitle } from "@mui/material";
+import {
+  Box,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Tab,
+  Tabs,
+} from "@mui/material";
 import { useRouter, usePathname } from "next/navigation";
 import { PrintButtons } from "@/app/(private)/order-detail/components/print-buttons";
 import { Info } from "@/app/(private)/order-detail/components/info";
 import { StatusBar } from "@/app/(private)/order-detail/components/status-bar";
 import { Facades } from "@/app/(private)/order-detail/components/facades";
+import { useState } from "react";
 
 interface OrderDetailClientProps {
   order: NonNullable<OrderDetailType>;
@@ -19,19 +27,31 @@ interface OrderDetailClientProps {
 function OrderDetailClient({ statuses, order }: OrderDetailClientProps) {
   const { push } = useRouter();
   const pathname = usePathname();
-
+  const [tabIndex, setTabIndex] = useState(0);
   const onClose = () => push(pathname);
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setTabIndex(newValue);
+  };
 
   return (
     <Dialog open onClose={onClose} maxWidth="lg" fullWidth>
       <DialogTitle>Детали заказа №{order.orderNumber}</DialogTitle>
       <DialogContent>
-        <Box position="relative">
-          <Info order={order} />
-          <Facades order={order} />
-          <StatusBar statuses={statuses} order={order} onClose={onClose} />
-          <PrintButtons order={order} />
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs value={tabIndex} onChange={handleChange}>
+            <Tab label="О заказе" />
+            <Tab label="Чат" />
+            <Tab label="Табель" />
+          </Tabs>
         </Box>
+        {tabIndex === 0 && (
+          <>
+            <Info order={order} />
+            <Facades order={order} />
+            <StatusBar statuses={statuses} order={order} onClose={onClose} />
+          </>
+        )}
+        <PrintButtons order={order} />
       </DialogContent>
     </Dialog>
   );
