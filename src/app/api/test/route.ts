df949536027964e4130ec9b0fd8178979aa69c73
ivandmitrackov <dmitrachkovivan@yandex.ru>
2025-11-04 10:56:13 +0300

    обернул всю ручку в try

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -2,8 +2,12 @@ import { NextResponse } from "next/server";
 
 export const runtime = "nodejs";
 
-export async function GET() {
+export async function POST(req: Request) {
+  const { login } = await req.json();
+
   return NextResponse.json({
     test: "success",
+    url: req.url,
+    login,
   });
 }
