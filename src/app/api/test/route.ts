7dabf2be651dc79c19f4013ff6680babe0537d25
ivandmitrackov <dmitrachkovivan@yandex.ru>
2025-11-04 14:44:23 +0300

    добавил для теста

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,13 +1,17 @@
 import { NextResponse } from "next/server";
+import { prisma } from "@/prisma-helpers/prisma";
 
 export const runtime = "nodejs";
 
 export async function POST(req: Request) {
   const { login } = await req.json();
 
+  const user = await prisma.user.findFirst({});
+
   return NextResponse.json({
     test: "success",
     url: req.url,
     login,
+    user,
   });
 }
